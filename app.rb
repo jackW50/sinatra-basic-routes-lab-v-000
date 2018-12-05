8713f99e3d7083afd992b657b76f1d264640b069
John Werenski <jtwerenski@gmail.com>
2018-12-05 15:56:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+     #resp.write item.price
+    
+  end 
+  
+  get '/hometown' do
+    
+  end 
+  
+  get '/favorite-song' do 
+    
+  end 
 end
